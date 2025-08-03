3bd95872017f86e88dfc0b5d2ff75cc4ba21f1ef
Noah Amoo <yawnoah3n@yahoo.com>
2025-08-03 01:42:29 +0100

    Applied Styles and added Images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const app = () => {
   return (
-    <View>
-      <Text>Coffee Shop</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Coffee Shop</Text>
     </View>
   )
 }
