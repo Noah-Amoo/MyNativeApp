1bb1def7c46fe474587758c8627290956de2b5eb
Noah Amoo <yawnoah3n@yahoo.com>
2025-08-03 14:08:38 +0100

    Worked on the contact page

--- app/(tabs)/contact.jsx
+++ app/(tabs)/contact.jsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text } from 'react-native'
+import { ThemedText } from '@/components/ThemedText'
 
 const contact = () => {
   return (
     <View>
-      <Text>contact</Text>
+      <ThemedText>Contact The Coffee Shop</ThemedText>
+      <Text>Or send in your details</Text>
     </View>
   )
 }
