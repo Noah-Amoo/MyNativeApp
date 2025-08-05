8cff933d581303d5a2c2235faadc7a91ab32923c
Noah Amoo <yawnoah3n@yahoo.com>
2025-08-05 18:18:45 +0100

    Updated the contact screen

--- app/contact.jsx
+++ app/contact.jsx
@@ -1,14 +1,97 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { ThemedText } from '@/components/ThemedText'
 
-const contact = () => {
+import { Fontisto } from '@expo/vector-icons';
+import {
+  StyleSheet, SafeAreaView, View, Text, Appearance
+} from 'react-native';
+import { Link } from 'expo-router';
+
+import { Colors } from '@/constants/Colors';
+
+export default function ContactScreen() {
+  const colorScheme = Appearance.getColorScheme();
+
+  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light;
+
+  const imgColor = colorScheme === 'dark' ? 'papayawhip' : '#333';
+
+  const styles = createStyles(theme, colorScheme);
+
   return (
-    <View>
-      <ThemedText>Contact The Coffee Shop</ThemedText>
-      <Text>Or send in your details</Text>
-    </View>
-  )
+
+    <SafeAreaView style={styles.container}>
+
+      <View style={styles.imgContainer}>
+        <Fontisto
+          name="coffeescript"
+          size={250}
+          color={imgColor}
+        />
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Noah Amoo</Text>
+
+        <View style={styles.textView}>
+          <Text style={styles.text}>
+            <Text>9 Strathcarron Court,</Text>{'\n'}
+            <Text>Cambridge, CB4 2UZ</Text>
+          </Text>
+        </View>
+
+        <View style={styles.textView}>
+          <Text style={styles.text}>
+            Phone:{'\n'}
+            <Link href="tel:07904637559" style={styles.link}>07904637559</Link>{'\n'}
+            or{' '}
+            <Link href="sms:07904637559" style={styles.link}>Click Here to Text!</Link>
+          </Text>
+        </View>
+
+        <View style={styles.textView}>
+          <Text style={styles.text}>
+            Hours:{'\n'}
+            <Text>Open 6am to 5pm daily.</Text>{'\n'}
+          </Text>
+        </View>
+
+      </View>
+
+    </SafeAreaView>
+  );
 }
 
-export default contact
+function createStyles(theme, colorScheme) {
+  return StyleSheet.create({
+    container: {
+      backgroundColor: theme.background,
+      paddingTop: 0,
+      flexGrow: 1
+    },
+    imgContainer: {
+      backgroundColor: colorScheme === 'dark' ? '#353636' : '#D0D0D0',
+      height: 250,
+    },
+    textContainer: {
+      backgroundColor: theme.background,
+      padding: 12,
+    },
+    title: {
+      color: theme.text,
+      fontSize: 24,
+      fontWeight: 'bold',
+      lineHeight: 32,
+      marginBottom: 10,
+    },
+    textView: {
+      marginBottom: 10,
+    },
+    text: {
+      color: theme.text,
+      fontSize: 16,
+      lineHeight: 24,
+    },
+    link: {
+      textDecorationLine: 'underline',
+    }
+  });
+}
